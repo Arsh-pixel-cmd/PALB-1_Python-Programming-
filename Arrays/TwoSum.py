078b2afaf47a34a131f9ca40d318b4559322bc3b
Arsh Mishra <arshm0312@gmail.com>
2026-02-15 22:15:10 +0530

    Add implementations for Minimum Jumps and Two Sum algorithms

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            rem = target - num
+            if rem in seen:
+                return [seen[rem], i]
+            seen[num] = i
